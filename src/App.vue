f33f4fdffd7182912f5727f4a57a3f879e037b80
Cauê Felipe de Souza Brito <150241018+Cauefrontdev@users.noreply.github.com>
2024-09-27 20:20:25 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,9 @@ onMounted(() => {
 
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
 
+
+
+
 /* Configura rolagem suave */
 html {
   scroll-behavior: smooth; 
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="carrousel-container">
+    <div style="overflow-x: hidden;" class="carrousel-container">
       <div id="carouselExampleCaptions" class="carousel slide">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
